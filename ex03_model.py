49ae834be6dcc0aa6077ac75b758dd383e5f067c
ta46tuty <ludwig.roggenkamp@fau.de>
2024-01-08 13:02:07 +0100

    third one

--- ex03_model.py
+++ ex03_model.py
@@ -1,5 +1,6 @@
 import torch
 import torch.nn as nn
+import torch.nn.functional as F
 
 
 class Swish(nn.Module):
@@ -12,31 +13,34 @@ class ShallowCNN(nn.Module):
         super().__init__()
         c_hid1 = hidden_features
         c_hid2 = hidden_features * 2
-        c_hid3 = hidden_features * 4
+        self.c_hid3 = hidden_features * 4
 
         self.cnn_layers = nn.Sequential(
             nn.Conv2d(1, c_hid1, kernel_size=5, stride=2, padding=4),
             Swish(),
             nn.Conv2d(c_hid1, c_hid2, kernel_size=3, stride=2, padding=1),
             Swish(),
-            nn.Conv2d(c_hid2, c_hid3, kernel_size=3, stride=2, padding=1),
+            nn.Conv2d(c_hid2, self.c_hid3, kernel_size=3, stride=2, padding=1),
             Swish(),
-            nn.Conv2d(c_hid3, c_hid3, kernel_size=3, stride=2, padding=1),
+            nn.Conv2d(self.c_hid3, self.c_hid3, kernel_size=3, stride=2, padding=1),
             Swish(),
-            nn.Conv2d(c_hid3, c_hid3, kernel_size=3, stride=2, padding=1),
+            nn.Conv2d(self.c_hid3, self.c_hid3, kernel_size=3, stride=2, padding=1),
             Swish(),
         )
         self.fc_layers = nn.Sequential(
             nn.Flatten(),
-            nn.Linear(c_hid3, num_classes)
+            nn.Linear(self.c_hid3, num_classes)
         )
 
+    @torch.no_grad()
     def get_logits(self, x):
-        # TODO (3.2): Implement classification procedure that outputs the logits across the classes
-        pass
+        return self.fc_layers(F.adaptive_avg_pool2d(self.cnn_layers(x), self.c_hid3))
 
     def forward(self, x, y=None) -> torch.Tensor:
         # TODO (3.2): Implement forward function for (1) EBM, (2) Unconditional JEM, (3) Conditional JEM.
         #  Consider using F.adaptive_avg_pool2d to convert between the 2D features and a linear representation.
         #  (You can also reuse your implementation of 'self.get_logits(x)' if this helps you.)
+        x = self.get_logits(x)
+        if y is None:
+            torch.logsumexp(x)
         pass
